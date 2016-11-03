943292a35d3bdadfd13a4997f1fd4c7d05f9bf1c
Yubraj Shahi <yubrajshahi@iMac.local>
2016-11-03 15:11:16 +0545

    completed the lazy loader

--- app/assets/scripts/app.js
+++ app/assets/scripts/app.js
@@ -1,3 +1,9 @@
 import MobileMenu from './modules/MobileMenu';
+import RevealOnScroll from './modules/revealOnScroll';
+import $ from 'jquery';
 
 var mobileMenu = new MobileMenu();
+//var revealOnScroll = new RevealOnScroll();
+
+new RevealOnScroll($(".feature-item"), "85%");
+new RevealOnScroll($(".testimonial"), "60%");
