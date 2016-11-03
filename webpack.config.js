4218b14b1a5c0500a9ada8c3a8d9ce2fbf8ed5a7
Yubraj Shahi <yubrajshahi@iMac.local>
2016-11-03 13:34:30 +0545

    completed desktop and mobile header

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  entry: "./app/assets/scripts/app.js",
+  output: {
+    path: "./app/temp/scripts",
+    filename: "app.js"
+  },
+
+  module: {
+    loaders: [
+      {
+        loader: 'babel',
+        query: {
+          presets: ['es2015']
+        },
+        test: /\.js$/,
+        exclude: /node_modules/
+      }
+    ]
+  }
+
+}
